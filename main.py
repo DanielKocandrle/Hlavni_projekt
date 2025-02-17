788fc6e3d7cae0d88519b6f9a979a56435c05b36
DanielKocandrle <daniel.kocandrle@student.ossp.cz>
2025-02-17 14:22:28 +0100

    slouceni login.html a success.html v main.py

--- main.py
+++ main.py
@@ -6,21 +6,26 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
 @app.route("/home")
 def index():
     return render_template("index.html")
 
+
 @app.route("/base")
 def base():
     return render_template("base.html")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         username = request.form['username']
         password = request.form['password']
-        return render_template("success.html", username=username, password=password)
+        return render_template("login.html", username=username, password=password)
     return render_template("login.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
