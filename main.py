bde162ad18bfd493d33e54fc421bd337c56fe5b5
DanielKocandrle <daniel.kocandrle@student.ossp.cz>
2025-03-17 12:58:19 +0100

    pridani databaoveho propojeni do main.py

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from flask import render_template, request, redirect, flash, url_for
 
-from app import app, login
+from app.auth import bp as auth_bp
 
-app.register_blueprint(login.bp)
+from app import app
+
+from os import path
+
+from app.db import create_db
 
 @app.route("/base")
 def base():
     return render_template("base.html")
 
 if __name__ == "__main__":
+    if not path.exists(app.config["DATABASE"]):
+        print("inicializace databaze")
+        with app.app_context():
+            create_db()
+
+    app.register_blueprint(auth_bp, url_prefix="/auth")
     app.run(debug=True)
