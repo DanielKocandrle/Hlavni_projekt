a64bbea9d1969ce5eba0b1e4e391d1f7377b0ef3
DanielKocandrle <daniel.kocandrle@student.ossp.cz>
2025-03-17 13:03:11 +0100

    pridani databaze do init

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+from os import path
+from app.db import create_db
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config["SECRET_KEY"] = "tajny_klic"
+app.config["DATABASE"] = "database.sqlite"
+app.config["DB_SCHEME"] = "scheme.sql"
 
 @app.route("/")
 def index():
