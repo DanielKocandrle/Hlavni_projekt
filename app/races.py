2fc52fda95d518ff461c8353b979d9775ce13ed1
DanielKocandrle <daniel.kocandrle@student.ossp.cz>
2025-04-14 13:42:11 +0200

    mazani by id

--- app/races.py
+++ app/races.py
@@ -24,8 +24,9 @@ def races():
 
         user_id = user_result[0][0]  # Přístup k první hodnotě v první n-tici
 
+        # Uložení závodu s user_id
         command = "INSERT INTO races (name, description, user_id) VALUES (?, ?, ?)"
-        execute(command, (name, description, user_id))
+        execute(command, (name, description, session['user']))  # použijeme session['user'] jako user_id
         flash("Závod byl úspěšně vytvořen.", "success")
         return redirect(url_for("races.races"))
 
@@ -47,15 +48,16 @@ def delete_race(race_id):
 
     # Získání závodu
     command = "SELECT user_id FROM races WHERE id = ?"
-    result = execute("SELECT user_id FROM races WHERE id = ?", (race_id,))
+    result = execute(command, (race_id,))
 
-    if not result or not result[0]:
+    if not result:
         flash("Závod neexistuje.", "danger")
         return redirect(url_for("races.races"))
 
-    race_creator = result[0][0]  # Získá user_id
+    race_creator = result[0][0]  # user_id, který vytvořil závod
 
-    if role == 'admin' or race_creator == session['user']:
+    # Admin může mazat všechny závody, nebo pokud uživatel vytvořil závod
+    if role == 'admin' or race_creator == user:
         execute("DELETE FROM races WHERE id = ?", (race_id,))
         flash("Závod byl úspěšně smazán.", "success")
     else:
